a57c34abb1933309f7f240dc69ce6342206bd5f9
prateek <stupidcoder22@gmail.com>
2022-06-21 15:38:27 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,65 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect, useState } from "react";
 import "./App.css";
 function App() {
+  const apiKey = "f56f24967aaf51182d1d4df628297c6d";
+  const [inputCity, setInputCity] = useState("");
+  const [data, setData] = useState({});
+
+  const weatherapi = async (city) => {
+    if (!city) return;
+    const apiURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&appid=" +
+      apiKey;
+    const data = await fetch(apiURL);
+    const res = await data.json();
+    setData(res);
+  };
+
+  const handleChange = (e) => {
+    setInputCity(e.target.value);
+  };
+
+  const handleSearch = () => {
+    weatherapi(inputCity);
+  };
+
   return (
     <div className="col-md-12">
       <div className="weatherBg">
         <h1 className="heading">Weather App</h1>
         <div className="d-grid gap-3 col-4 mt-4">
-          <input type="text" className="form-control" />
-          <button className="btn btn-primary" type="button">
+          <input
+            type="text"
+            className="form-control"
+            onChange={handleChange}
+            value={inputCity}
+          />
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={handleSearch}
+          >
             Search
           </button>
         </div>
       </div>
+
+      {Object.keys(data).length > 0 && (
+        <div className="col-md-12 text-center mt-5">
+          <div className="shadow rounded wetherResultBox">
+            <img
+              className="weathorIcon"
+              src="https://i.pinimg.com/originals/77/0b/80/770b805d5c99c7931366c2e84e88f251.png"
+            />
+            <h5 className="weathorCity">{data.name}</h5>
+            <h6 className="weathorTemp">
+              {(data.main.temp - 273.15).toFixed(2)}°C
+            </h6>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
